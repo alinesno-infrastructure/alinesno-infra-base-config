c5473a7a77bb7ea8c1d60be5318e810b2b131a37
switchII <luoandon@gmail.com>
2024-07-07 11:28:39 +0800

    feat:调整审计类

--- alinesno-infra-base-config-domain/src/main/java/com/alinesno/infra/base/config/service/impl/ConfigAuditServiceImpl.java
+++ alinesno-infra-base-config-domain/src/main/java/com/alinesno/infra/base/config/service/impl/ConfigAuditServiceImpl.java
@@ -1,13 +1,11 @@
 package com.alinesno.infra.base.config.service.impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
 import com.alinesno.infra.base.config.entity.ConfigAuditEntity;
 import com.alinesno.infra.base.config.mapper.ConfigAuditMapper;
 import com.alinesno.infra.base.config.service.IConfigAuditService;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 /**
  * <p>
@@ -17,11 +15,9 @@ import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
  * @author weixiaojin
  * @version 1.0.0
  */
+@Slf4j
 @Service
 public class ConfigAuditServiceImpl extends IBaseServiceImpl<ConfigAuditEntity, ConfigAuditMapper> implements IConfigAuditService {
 
-	// 日志记录
-	@SuppressWarnings("unused")
-	private static final Logger log = LoggerFactory.getLogger(ConfigAuditServiceImpl.class);
 
 }
