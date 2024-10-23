5ac8ecf616afff7717b4e391990cc91998e1b87a
李伟(Michael) <michaelAip@gmail.com>
2024-10-23 19:25:20 +0800

    feat:在 ConfigureCatalogController 中添加数据权限注解

--- alinesno-infra-base-config-gateway/src/main/java/com/alinesno/infra/base/config/api/controller/ConfigureCatalogController.java
+++ alinesno-infra-base-config-gateway/src/main/java/com/alinesno/infra/base/config/api/controller/ConfigureCatalogController.java
@@ -4,6 +4,10 @@ import com.alinesno.infra.base.config.entity.ConfigureCatalogEntity;
 import com.alinesno.infra.base.config.service.IConfigureCatalogService;
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
 import com.alinesno.infra.common.core.utils.StringUtils;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionQuery;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionSave;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.facade.response.AjaxResult;
@@ -43,6 +47,7 @@ public class ConfigureCatalogController extends BaseController<ConfigureCatalogE
      * @param page DatatablesPageBean对象。
      * @return 包含DataTables数据的TableDataInfo对象。
      */
+    @DataPermissionScope
     @ResponseBody
     @PostMapping("/datatables")
     public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
@@ -50,9 +55,10 @@ public class ConfigureCatalogController extends BaseController<ConfigureCatalogE
         return this.toPage(model, this.getFeign(), page);
     }
 
+    @DataPermissionQuery
     @GetMapping("/list")
-    public AjaxResult list(ConfigureCatalogEntity ConfigureCatalog) {
-        List<ConfigureCatalogEntity> ConfigureCatalogEntities = service.selectCatalogList(ConfigureCatalog);
+    public AjaxResult list(ConfigureCatalogEntity ConfigureCatalog , PermissionQuery query) {
+        List<ConfigureCatalogEntity> ConfigureCatalogEntities = service.selectCatalogList(ConfigureCatalog  , query);
 
         return AjaxResult.success("操作成功." , ConfigureCatalogEntities) ;
     }
@@ -61,6 +67,7 @@ public class ConfigureCatalogController extends BaseController<ConfigureCatalogE
      * 保存角色类型
      * @return
      */
+    @DataPermissionSave
     @PostMapping("/insertCatalog")
     public AjaxResult insertCatalog(@RequestBody ConfigureCatalogEntity entity){
 
@@ -74,10 +81,11 @@ public class ConfigureCatalogController extends BaseController<ConfigureCatalogE
      * @param deptId
      * @return
      */
+    @DataPermissionQuery
     @GetMapping("/excludeChild/{id}")
-    public AjaxResult excludeChild(@PathVariable(value = "id", required = false) Long deptId)
+    public AjaxResult excludeChild(@PathVariable(value = "id", required = false) Long deptId  , PermissionQuery query)
     {
-        List<ConfigureCatalogEntity> depts = service.selectCatalogList(new ConfigureCatalogEntity());
+        List<ConfigureCatalogEntity> depts = service.selectCatalogList(new ConfigureCatalogEntity(), query);
         depts.removeIf(d -> d.getId().longValue() == deptId || ArrayUtils.contains(StringUtils.split(d.getAncestors(), ","), deptId + ""));
         return AjaxResult.success("操作成功." , depts);
     }
