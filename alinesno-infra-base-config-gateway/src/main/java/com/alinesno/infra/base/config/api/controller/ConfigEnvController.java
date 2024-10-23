054d3b3cf0864689992c583a291c1084004b19d0
李伟(Michael) <michaelAip@gmail.com>
2024-10-23 19:25:20 +0800

    feat:在 ConfigEnvController 中添加数据权限注解及保存方法

--- alinesno-infra-base-config-gateway/src/main/java/com/alinesno/infra/base/config/api/controller/ConfigEnvController.java
+++ alinesno-infra-base-config-gateway/src/main/java/com/alinesno/infra/base/config/api/controller/ConfigEnvController.java
@@ -3,8 +3,11 @@ package com.alinesno.infra.base.config.api.controller;
 import com.alinesno.infra.base.config.entity.ConfigEnvEntity;
 import com.alinesno.infra.base.config.service.IConfigEnvService;
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionSave;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
+import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.rest.BaseController;
 import io.swagger.annotations.Api;
 import jakarta.servlet.http.HttpServletRequest;
@@ -43,11 +46,18 @@ public class ConfigEnvController extends BaseController<ConfigEnvEntity, IConfig
      * @param page DatatablesPageBean对象。
      * @return 包含DataTables数据的TableDataInfo对象。
      */
+    @DataPermissionScope
     @ResponseBody
     @PostMapping("/datatables")
     public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
         log.debug("page = {}", ToStringBuilder.reflectionToString(page));
         return this.toPage(model, this.getFeign(), page);
+    }
+
+    @DataPermissionSave
+    @Override
+    public AjaxResult save(Model model, ConfigEnvEntity entity) throws Exception {
+        return super.save(model, entity);
     }
 
     @Override
