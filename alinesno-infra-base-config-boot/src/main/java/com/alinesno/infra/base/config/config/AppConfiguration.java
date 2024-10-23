4e14c8c36a0b3a6c15a93f2ccd608b9f72731abd
李伟(Michael) <michaelAip@gmail.com>
2024-10-23 19:25:20 +0800

    添加 Forest 框架扫描配置及组件扫描配置

--- alinesno-infra-base-config-boot/src/main/java/com/alinesno/infra/base/config/config/AppConfiguration.java
+++ alinesno-infra-base-config-boot/src/main/java/com/alinesno/infra/base/config/config/AppConfiguration.java
@@ -4,10 +4,12 @@ import com.alinesno.infra.base.config.service.IInitDataService;
 import com.alinesno.infra.common.facade.enable.EnableActable;
 import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import com.alinesno.infra.common.web.log.aspect.LogAspect;
+import com.dtflys.forest.springboot.annotation.ForestScan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -17,6 +19,13 @@ import org.springframework.context.annotation.Configuration;
 @EnableInfraSsoApi
 @MapperScan("com.alinesno.infra.base.config.mapper")
 @Configuration
+@ForestScan({
+        "com.alinesno.infra.common.web.adapter.base.consumer"
+})
+@ComponentScan({
+        "com.alinesno.infra.base.config",
+        "com.alinesno.infra.common.extend.datasource"
+})
 public class AppConfiguration implements CommandLineRunner {
 
     @Autowired
