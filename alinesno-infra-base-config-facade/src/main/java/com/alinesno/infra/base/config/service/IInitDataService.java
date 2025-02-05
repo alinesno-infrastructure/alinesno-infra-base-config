6fcf1a4d940e387ace16136aea76c8bb2df90235
周健(James) <jamesAip@gmail.com>
2025-02-06 00:14:01 +0800

    refactor: 移除IInitDataService接口的注释以保持代码整洁

--- alinesno-infra-base-config-facade/src/main/java/com/alinesno/infra/base/config/service/IInitDataService.java
+++ alinesno-infra-base-config-facade/src/main/java/com/alinesno/infra/base/config/service/IInitDataService.java
@@ -1,26 +1,26 @@
-package com.alinesno.infra.base.config.service;
-
-/**
- * 初始化管理配置中心示例数据，用于完善项目
- */
-public interface IInitDataService {
-
-    /**
-     * 初始化组织环境
-     * @param groupId
-     */
-    void initEnv(long groupId) ;
-
-    /**
-     * 初始化分组级别
-     * @param groupId
-     */
-    void initCatalog(long groupId) ;
-
-    /**
-     * 初始化示例数据
-     * @param groupId
-     */
-    void initDemo(long groupId) ;
-
-}
+//package com.alinesno.infra.base.config.service;
+//
+///**
+// * 初始化管理配置中心示例数据，用于完善项目
+// */
+//public interface IInitDataService {
+//
+//    /**
+//     * 初始化组织环境
+//     * @param groupId
+//     */
+//    void initEnv(long groupId) ;
+//
+//    /**
+//     * 初始化分组级别
+//     * @param groupId
+//     */
+//    void initCatalog(long groupId) ;
+//
+//    /**
+//     * 初始化示例数据
+//     * @param groupId
+//     */
+//    void initDemo(long groupId) ;
+//
+//}
